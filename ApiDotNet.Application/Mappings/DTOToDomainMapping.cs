fd33287d091435e4e7af564182f05e5ab6ca491f
victor brambilla <victor-brambilla@hotmail.com>
2023-05-15 18:30:58 -0400

    feat: Create mappings

---
+++ ApiDotNet.Application/Mappings/DTOToDomainMapping.cs
@@ -0,0 +1,19 @@
+﻿using ApiDotNet.Application.DTOs;
+using ApiDotNet.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDotNet.Application.Mappings
+{
+    public class DTOToDomainMapping : Profile
+    {
+        public DTOToDomainMapping()
+        {
+            CreateMap<PersonDTO, Person>();
+        }
+    }
+}
