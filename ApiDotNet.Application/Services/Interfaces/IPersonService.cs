518e4b15a93dd8e5a694f895d689f6ad9f93ff93
victor brambilla <victor-brambilla@hotmail.com>
2023-05-15 18:31:27 -0400

    feat: Create person service

---
+++ ApiDotNet.Application/Services/Interfaces/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using ApiDotNet.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDotNet.Application.Services.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<ResultService<PersonDTO>> CreateAsync(PersonDTO person);
+    }
+}
---
+++ ApiDotNet.Application/Services/PersonService.cs
@@ -0,0 +1,42 @@
+﻿using ApiDotNet.Application.DTOs;
+using ApiDotNet.Application.DTOs.Validations;
+using ApiDotNet.Application.Services.Interfaces;
+using ApiDotNet.Domain.Entities;
+using ApiDotNet.Domain.Repositories;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDotNet.Application.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository _personRepository;
+        private readonly IMapper _mapper;
+        public PersonService(IPersonRepository personRepository, IMapper mapper) 
+        {
+            _mapper = mapper;
+            _personRepository = personRepository;
+        }
+        public async Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO)
+        {
+            if(personDTO == null)
+            {
+                return  ResultService.Fail<PersonDTO>("Objeto deve ser informado");
+            }
+
+            var result = new PersonDTOValidator().Validate(personDTO);
+            if(!result.IsValid)
+            {
+                return ResultService.RequestError<PersonDTO>("Problemas de validação!", result);
+            }
+
+            var person = _mapper.Map<Person>(personDTO);
+            var data = await _personRepository.CreateAsync(person);
+            return ResultService.Ok<PersonDTO>(_mapper.Map<PersonDTO>(data));  
+        }
+    }
+}
