5481696b3401c7d39d64770ef13fd5bbcfdaf51f
victor brambilla <victor-brambilla@hotmail.com>
2023-05-15 19:32:25 -0400

    feat: Create post method of person

---
+++ ApiDotNet.Api/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using ApiDotNet.Application.DTOs;
+using ApiDotNet.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDotNet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] PersonDTO personDTO)
+        {
+            var result = await _personService.CreateAsync(personDTO);
+            if(result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
